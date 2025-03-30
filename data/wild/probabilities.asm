da0f9708382e9064551477391dc80d5fd23ca9cf
Cubatman <triley1196@gmail.com>
2025-03-30 18:47:11 -0400

    added 1 wild grass slot, added LittleWater encounters

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -5,13 +5,14 @@ ENDM
 
 GrassMonProbTable:
 	table_width 2
-	mon_prob 30,  0 ; 30% chance
-	mon_prob 60,  1 ; 30% chance
-	mon_prob 80,  2 ; 20% chance
-	mon_prob 90,  3 ; 10% chance
-	mon_prob 95,  4 ;  5% chance
-	mon_prob 99,  5 ;  4% chance
-	mon_prob 100, 6 ;  1% chance
+	mon_prob 25,  0 ; 25% chance
+	mon_prob 50,  1 ; 25% chance
+	mon_prob 65,  2 ; 15% chance
+	mon_prob 75,  3 ; 10% chance
+	mon_prob 85,  4 ; 10% chance
+	mon_prob 92,  5 ; 7% chance
+	mon_prob 97,  6 ; 5% chance
+	mon_prob 100, 7 ; 3% chance
 	assert_table_length NUM_GRASSMON
 
 WaterMonProbTable:

3361eb654254d6e22e98c85e35cacbe5898543b2
Cubatman <triley1196@gmail.com>
2025-04-12 23:54:53 -0400

    added pokemart to little water

--- data/items/marts.asm
+++ data/items/marts.asm
@@ -35,14 +35,31 @@ Marts:
 	dw MartMtMoon
 	dw MartIndigoPlateau
 	dw MartUnderground
+	dw MartLittleWaterSpecial1
+	dw MartLittleWaterSpecial2
 	assert_table_length NUM_MARTS
 
-MartCherrygrove:
-	db 4 ; # items
+MartLittleWaterSpecial1: ;before beating gym
+	db 2 ; # items
+	db TM_ROAR
+	db TM_HEADBUTT
+	db -1 ; end
+
+MartLittleWaterSpecial2: ;after beating gym
+	db 3 ; # items
+	db TM_ROAR
+	db TM_HEADBUTT
+	db TM_MUD_SLAP
+	db -1 ; end
+
+MartCherrygrove: ; Little Water
+	db 6 ; # items
 	db POTION
+	db POKE_BALL
 	db ANTIDOTE
 	db PARLYZ_HEAL
 	db AWAKENING
+	db ESCAPE_ROPE
 	db -1 ; end
 
 MartCherrygroveDex:
--- maps/LittleWater/LittleWaterCenter.asm
+++ maps/LittleWater/LittleWaterCenter.asm
@@ -1,6 +1,8 @@
     object_const_def
 ;	const MAPNAME_OBJECTNAME
     const LITTLEWATER_NURSE1
+	const LITTLEWATER_CLERK1
+	const LITTLEWATER_CLERK2
 
 
 LittleWaterCenter_MapScripts:
@@ -23,10 +25,33 @@ LittleWaterCenter_MapEvents:
 
 	def_bg_events
 ;	bg_event x, y, type, script
+	bg_event 6, 2, OBJECTTYPE_SCRIPT, LittleWaterClerkScript
+	bg_event 7, 2, OBJECTTYPE_SCRIPT, LittleWaterSpecialClerkScript
 
 	def_object_events
 ;	object_event x, y, sprite, movement, rx, ry, h1, h2, palette, type, range, script, event_flag
     object_event 3, 1, SPRITE_NURSE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, LittleWaterNurseScript, -1
+	object_event 6, 1, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, LittleWaterClerkScript, -1
+	object_event 7, 1, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, LittleWaterSpecialClerkScript, -1
+
+LittleWaterClerkScript:
+	opentext
+	pokemart MARTTYPE_STANDARD, MART_CHERRYGROVE
+	closetext
+	end
+
+LittleWaterSpecialClerkScript:
+	checkevent EVENT_BEAT_BROCK
+	iftrue .ImprovedMart
+	opentext 
+	pokemart MARTTYPE_STANDARD, MART_LITTLE_WATER_SPECIAL_1
+	closetext
+	end
+.ImprovedMart:
+	opentext 
+	pokemart MARTTYPE_STANDARD, MART_LITTLE_WATER_SPECIAL_2
+	closetext
+	end
 
 LittleWaterNurseScript:   
     jumpstd PokecenterNurseScript

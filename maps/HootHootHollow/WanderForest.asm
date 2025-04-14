087b23cc39b9adc1de8affe1f05f3a9bd485384c
Cubatman <triley1196@gmail.com>
2025-04-14 19:55:31 -0400

    commiting good hoot-hoot-hollow files

---
+++ maps/HootHootHollow/WanderForest.asm
@@ -0,0 +1,19 @@
+    object_const_def
+
+WanderForest_MapScripts:
+    def_scene_scripts
+    
+    def_callbacks
+
+WanderForest_MapEvents:
+    db 0, 0 ; filler
+
+    def_warp_events
+    warp_event 11, 3, LITTLE_WATER_HOLLOW, 1
+    warp_event 12, 3, LITTLE_WATER_HOLLOW, 1
+
+    def_coord_events
+
+    def_bg_events
+
+    def_object_events
